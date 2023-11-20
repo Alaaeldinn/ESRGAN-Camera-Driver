e5053ff6b64646cda443b9f8062b7ff94298133b
thealaaeldin <116704618+Alaaeldinn@users.noreply.github.com>
2023-11-20 19:15:38 +0000

    Update esrgan_node.py

--- ROS/esrgan_ros/src/esrgan_node.py
+++ ROS/esrgan_ros/src/esrgan_node.py
@@ -8,82 +8,87 @@ import numpy as np
 import torch
 from RealESRGAN import RealESRGAN
 from PIL import Image as PILImage
-from multiprocessing import Process, Queue
+import signal
+import logging
 
 class ESRGANManager:
-    _instance = None
-
-    def __new__(cls, *args, **kwargs):
-        if not cls._instance:
-            cls._instance = super(ESRGANManager, cls).__new__(cls, *args, **kwargs)
-            # Load the ESRGAN model only once
-            cls._instance._load_model()
-        return cls._instance
-
-    def _load_model(self):
+    def __init__(self, model_path='/path/to/weights/RealESRGAN_x4plus.pth'):
         device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         self.model = RealESRGAN(device, scale=4)
-        self.model.load_weights('/path/to/weights/RealESRGAN_x4plus.pth', download=True)
+        self.model.load_weights(model_path, download=True)
 
     def predict(self, pil_frame):
         return self.model.predict(pil_frame)
 
-def capture_frames(cap, esrgan_manager, bridge, image_queue, rate):
-    while not rospy.is_shutdown():
-        ret, frame = cap.read()
+def capture_frame(cap, esrgan_manager, bridge, image_queue, rate):
+    try:
+        while not rospy.is_shutdown():
+            ret, frame = cap.read()
 
-        if not ret:
-            break
+            if not ret:
+                break
 
-        # Convert to PIL Image directly without using cv2
-        pil_frame = PILImage.fromarray(frame)
+            pil_frame = PILImage.fromarray(frame)
 
-        sr_image = esrgan_manager.predict(pil_frame)
-        enhanced_frame = np.array(sr_image)
+            sr_image = esrgan_manager.predict(pil_frame)
+            enhanced_frame = np.array(sr_image)
 
-        ros_enhanced_frame = bridge.cv2_to_imgmsg(enhanced_frame, encoding="bgr8")
-        image_queue.put(ros_enhanced_frame)
+            # Put the enhanced frame into the queue
+            image_queue.put(enhanced_frame)
 
-        rate.sleep()
+            rate.sleep()
 
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+    except Exception as e:
+        logging.error(f"Error in capture_frame: {e}")
+    finally:
+        cap.release()
 
-    cap.release()
+def publish_frame(image_queue, image_pub, rate):
+    try:
+        while not rospy.is_shutdown():
+            # Get the enhanced frame from the queue
+            enhanced_frame = image_queue.get()
 
-def publish_frames(image_queue, image_pub):
-    while not rospy.is_shutdown():
-        if not image_queue.empty():
-            ros_enhanced_frame = image_queue.get()
+            ros_enhanced_frame = CvBridge().cv2_to_imgmsg(enhanced_frame, encoding="bgr8")
             image_pub.publish(ros_enhanced_frame)
+
+            rate.sleep()
+
+    except Exception as e:
+        logging.error(f"Error in publish_frame: {e}")
+
+def shutdown_handler(signum, frame):
+    rospy.signal_shutdown("Received shutdown signal")
 
 def main():
     rospy.init_node('esrgan_node', anonymous=True)
     rate = rospy.Rate(30)
 
-    esrgan_manager = ESRGANManager()
-
-    # Adjust capture settings based on your needs
-    cap = cv2.VideoCapture(0)
-    bridge = CvBridge()
-    image_pub = rospy.Publisher('esrgan_frame', Image, queue_size=10)
-    image_queue = Queue()
-
-    capture_process = Process(target=capture_frames, args=(cap, esrgan_manager, bridge, image_queue, rate))
-    capture_process.start()
-
-    publish_process = Process(target=publish_frames, args=(image_queue, image_pub))
-    publish_process.start()
+    signal.signal(signal.SIGINT, shutdown_handler)
+    signal.signal(signal.SIGTERM, shutdown_handler)
 
     try:
+        esrgan_manager = ESRGANManager()
+
+        cap = cv2.VideoCapture(0)
+        image_queue = Queue()  # Use multiprocessing.Queue for inter-process communication
+        image_pub = rospy.Publisher('esrgan_frame', Image, queue_size=10)
+
+        # Start the capture_frame and publish_frame processes
+        capture_process = Process(target=capture_frame, args=(cap, esrgan_manager, CvBridge(), image_queue, rate))
+        capture_process.start()
+
+        publish_process = Process(target=publish_frame, args=(image_queue, image_pub, rate))
+        publish_process.start()
+
         rospy.spin()
-    except KeyboardInterrupt:
+
+    except rospy.ROSInterruptException:
         pass
-
-    capture_process.join()
-    publish_process.join()
-
-    cv2.destroyAllWindows()
+    finally:
+        capture_process.join()
+        publish_process.join()
+        cv2.destroyAllWindows()
 
 if __name__ == "__main__":
     main()
